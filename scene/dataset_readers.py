ea7b9023ca250c811e176826d98714c9066ab80d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-04-16 18:23:22 +0200

    Added training script

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -258,6 +258,7 @@ def readCamerasFromTransforms(path, transformsfile, white_background, extension=
             R = np.transpose(w2c[:3,:3])  # R is stored transposed due to 'glm' in CUDA code
             T = w2c[:3, 3]
 
+            print(cam_name)
             image_path = os.path.join(path, 'small_harbour_sunset_4k_32x16_rot270', cam_name)
             image_name = Path(cam_name).stem
             print('!!! ', image_path, cam_name)
