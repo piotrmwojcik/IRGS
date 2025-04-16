628f311b9adb693be86adbefa4780602a6e02ce8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-04-16 22:49:19 +0200

    one view

--- train_refgaussian.py
+++ train_refgaussian.py
@@ -120,7 +120,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         # Pick a random Camera
         if not viewpoint_stack:
             viewpoint_stack = scene.getTrainCameras().copy()
-        viewpoint_cam = 0 #viewpoint_stack.pop(randint(0, len(viewpoint_stack) - 1))
+        viewpoint_cam = viewpoint_stack[0]
 
         # Set render
         render = select_render_method(iteration, opt, initial_stage)
