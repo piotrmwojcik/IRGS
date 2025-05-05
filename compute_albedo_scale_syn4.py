370c9eb5aafa848630ba4203e52cbb37c9b806c7
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-05 22:39:11 +0200

    one view

--- compute_albedo_scale_syn4.py
+++ compute_albedo_scale_syn4.py
@@ -95,7 +95,7 @@ if __name__ == '__main__':
 
         albedo_path = match
         #print('!!!! ', albedo_path)
-        gt_albedo_np = load_img_rgb(albedo_path)
+        gt_albedo_np = load_img_rgb(os.path.join(args.source_path, 'albedo', match))
         mask = torch.from_numpy(gt_albedo_np[..., 3:4]).permute(2, 0, 1).float().cuda()
         gt_albedo = torch.from_numpy(gt_albedo_np[..., :3] * gt_albedo_np[..., 3:4]).permute(2, 0, 1).float().cuda()
         
