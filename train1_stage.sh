91bdfdb5b237fda0989f95c95494ebf111b6acec
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-06 13:48:00 +0200

    one view

---
+++ train1_stage.sh
@@ -0,0 +1,21 @@
+for SCENE in \
+    hook150_v3_transl_statictimestep1 \
+    jumpingjacks150_v3_tex_statictimestep75 \
+    mouse150_v2_transl_statictimestep1 \
+    spheres_cube_dataset_v5_statictimestep1 \
+    standup150_v3_statictimestep75
+do
+    for DATA_SUBDIR in \
+        chapel_day_4k_32x16_rot0 \
+        dam_wall_4k_32x16_rot90 \
+        golden_bay_4k_32x16_rot30
+    do
+        export DATA_SUBDIR
+        echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
+
+        CUDA_VISIBLE_DEVICES=0 python train_refgaussian.py \
+            -s data/$SCENE \
+            -m outputs/$SCENE/$DATA_SUBDIR \
+            --eval -w --lambda_mask_entropy 0.05 --resolution 2
+    done
+done
