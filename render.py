17f0ef972db9bbcdfd233c2d5e1b002a3aa421ea
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-07 00:20:30 +0200

    one view

--- render.py
+++ render.py
@@ -92,7 +92,7 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-        env_dict = gaussians.render_env_map_2()
+        env_dict = gaussians.render_env_map()
 
         grid = [
             env_dict["env1"].permute(2, 0, 1),
