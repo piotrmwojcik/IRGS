c04646597c1e6fc134bbfd7133052c957cfe67bc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-09 22:29:36 +0200

    nvs

--- do_nvs.sh
+++ do_nvs.sh
@@ -14,6 +14,6 @@ do
         echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
 
         CUDA_VISIBLE_DEVICES=0 python render.py \
-            -m outputs/Synthetic4Relight/jugs/irgs --eval --diffuse_sample_num 1024
+            -m outputs/s2_${DATA_SUBDIR}/irgs_$SCENE --eval --diffuse_sample_num 1024
     done
 done
