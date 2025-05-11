5aee50e2a4e571e4fe257cf5d52ed9882cace7af
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 21:06:36 +0200

    nvs spheres v8

--- compute_albedo_scale_syn4.py
+++ compute_albedo_scale_syn4.py
@@ -111,7 +111,7 @@ if __name__ == '__main__':
         # Resize to 400x400 using bilinear interpolation
         import torch.nn.functional as F
         # Interpolate to [1, 1, 400, 400]
-        #print('before interpolate ', mask.shape)
+        print('before interpolate ', mask.shape, gt_albedo.shape)
         mask = F.interpolate(mask.unsqueeze(0), size=(400, 400), mode='bilinear', align_corners=False).squeeze(0)
         # Remove batch dimension: [1, 400, 400]
         gt_albedo = F.interpolate(gt_albedo.unsqueeze(0), size=(400, 400), mode='bilinear',
