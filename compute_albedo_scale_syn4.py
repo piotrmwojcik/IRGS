542f1b2da1446abea12bc32aa5c743ce2e6261c6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 10:47:40 +0200

    spheres v8

--- compute_albedo_scale_syn4.py
+++ compute_albedo_scale_syn4.py
@@ -99,10 +99,12 @@ if __name__ == '__main__':
         ### RESOLUTION
         from PIL import Image
         gt_albedo_np = load_img_rgb(os.path.join(args.source_path, 'albedo', match))
+        gt_albedo_np = srgb_to_linear(gt_albedo_np)
         img_pil = Image.fromarray((gt_albedo_np * 255).astype(np.uint8))  # if in [0, 1] float
         new_size = (int(img_pil.width * scale_factor), int(img_pil.height * scale_factor))
         img_pil = img_pil.resize(new_size, Image.BICUBIC)
         gt_albedo_np = np.array(img_pil) / 255.0  # back to float if needed
+        gt_albedo_np = linear_to_srgb(gt_albedo_np)
         print('!!! ', gt_albedo_np.shape)
         mask = torch.from_numpy(gt_albedo_np[..., 3:4]).permute(2, 0, 1).float().cuda()
         gt_albedo = torch.from_numpy(gt_albedo_np[..., :3] * gt_albedo_np[..., 3:4]).permute(2, 0, 1).float().cuda()
