76c7bf26d51f209f088df20d6db6e441ed7e68c8
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 17:46:29 +0200

    nvs spheres v8

---
+++ do_nvs_emg.sh
@@ -0,0 +1,19 @@
+for SCENE in \
+    hook150_v3_transl_statictimestep1 \
+    jumpingjacks150_v3_tex_statictimestep75 \
+    mouse150_v2_transl_statictimestep1 \
+    spheres_cube_dataset_v5_statictimestep1 \
+    standup150_v3_statictimestep75
+do
+    for DATA_SUBDIR in \
+        chapel_day_4k_32x16_rot0 \
+        dam_wall_4k_32x16_rot90 \
+        golden_bay_4k_32x16_rot330
+    do
+        export DATA_SUBDIR
+        echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
+
+        CUDA_VISIBLE_DEVICES=0 python render.py \
+            -m outputs/s2_${DATA_SUBDIR}/irgs_$SCENE --eval --diffuse_sample_num 1024
+    done
+done
