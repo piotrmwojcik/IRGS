933f6de42201996dcf7f30f2effcc6614f50172d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 20:42:37 +0200

    nvs spheres v8

--- compute_albedo_scale_syn4.py
+++ compute_albedo_scale_syn4.py
@@ -100,9 +100,9 @@ if __name__ == '__main__':
         from PIL import Image
 
         subdir = os.environ.get("DATA_SUBDIR", "")
-        gt_albedo_np = load_img_rgb(os.path.join(args.source_path, 'albedo', match)).permute(1, 2, 0)[:3]
+        gt_albedo_np = load_img_rgb(os.path.join(args.source_path, 'albedo', match))
         print('gt_albedo_np ', gt_albedo_np.shape)
-        gt_albedo = torch.from_numpy(gt_albedo_np).cuda()
+        gt_albedo = torch.from_numpy(gt_albedo_np.permute(1, 2, 0)[:3]).cuda()
         image_path = os.path.join(args.source_path, f'{subdir}/' + frame["file_path"].split("/")[-1] + ".png")
         image_rgba = load_img_rgb(image_path)
         print('loaded ', gt_albedo_np.shape, image_rgba.shape)
