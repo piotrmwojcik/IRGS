0d070e36ba6f3062d3ebcf577489410986f2142b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-11 13:38:42 +0200

    nvs spheres v8

--- eval_materials.sh
+++ eval_materials.sh
@@ -13,8 +13,8 @@ do
         export DATA_SUBDIR
         echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
 
-    CUDA_VISIBLE_DEVICES=0 CUDA_VISIBLE_DEVICES=0 python eval_material_tensoir.py -m \
-      outputs/s2_${DATA_SUBDIR}/irgs_$SCENE --albedo_rescale 2
+      CUDA_VISIBLE_DEVICES=0 CUDA_VISIBLE_DEVICES=0 python eval_material_syn4.py -m \
+        outputs/s2_${DATA_SUBDIR}/irgs_$SCENE --albedo_rescale 2
 
     done
 done
