bd15676b6916873d76bf62d32c31d96f8275f4b1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-12 21:01:54 +0200

    background

--- render.py
+++ render.py
@@ -61,7 +61,7 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
             if 'normal' in key:
                 mask_nonblack = (out != 0).any(dim=0, keepdim=True)  # shape: [1, H, W]
                 out = (out + 1) / 2
-                out = out * mask_nonblack
+                out = out * mask_nonblack + (1.0 * (~mask_nonblack))
             if 'position' in key:
                 out = (out + 1) / 2
             if out.shape[0] == 1:
