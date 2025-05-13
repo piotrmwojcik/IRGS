63b549f4aaf4f7605d24c4e935d726b0fb187093
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-13 22:09:12 +0200

    another relight

--- scene/__init__.py
+++ scene/__init__.py
@@ -117,7 +117,7 @@ class Scene:
             #                                            "point_cloud.ply"))
 
 
-            self.gaussians.load_ply(os.path("/home/pwojcik/IRGS/outputs/images_710_780_single_ts/point_cloud/iteration_50000/point_cloud.ply"))
+            self.gaussians.load_ply(os.path.join("/home/pwojcik/IRGS/outputs/images_710_780_single_ts/point_cloud/iteration_50000/point_cloud.ply"))
         else:
             self.gaussians.create_from_pcd(scene_info.point_cloud, self.cameras_extent, args)
 
