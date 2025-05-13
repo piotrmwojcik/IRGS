4d21b3f42d18befa6e88951c72b8a5bbdba2d732
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-13 19:23:05 +0200

    another relight

---
+++ train_stage2_enerf.sh
@@ -0,0 +1,17 @@
+SCENE=images_710_780_single_ts
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+    -s data/$SCENE \
+    --iterations 20000 \
+    --start_checkpoint_refgs outputs/$SCENE/chkpnt50000.pth \
+    --envmap_resolution 128 \
+    --lambda_base_color_smooth 2 \
+    --lambda_roughness_smooth 2 \
+    --diffuse_sample_num 256 \
+    --envmap_cubemap_lr 0.01 \
+    --lambda_light_smooth 0.0005 \
+    --init_roughness_value 0.6 \
+    --lambda_light 0.1 \
+    -m outputs/irgs_$SCENE \
+    --train_ray \
+    --resolution 2
