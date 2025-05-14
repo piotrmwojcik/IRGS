6f65d1fd1e8f151526f53d0ea305912f1a8ddeaa
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-14 20:25:25 +0200

    try to load from ply

--- do_relight_enerf.sh
+++ do_relight_enerf.sh
@@ -18,7 +18,7 @@ for MAP_NAME in "${maps[@]}"; do
 
   CUDA_VISIBLE_DEVICES=0 python eval_relighting_enerf.py -m "outputs/irgs_${SCENE}" \
     --diffuse_sample_num 1024 \
-    --light_sample_num 0 \
+    --light_sample_num 256 \
     --resolution 4 \
     --albedo_rescale 0 \
     -e light
