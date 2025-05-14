fa4463cb498a1407bf660c904a92c8141b53864d
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-14 16:28:32 +0200

    try to load from ply

--- do_nvs_enerf.sh
+++ do_nvs_enerf.sh
@@ -1,12 +1,4 @@
 SCENE="images_710_780_single_ts"
 
-for SCENE in \
-    hook150_v3_transl_statictimestep1 \
-    jumpingjacks150_v3_tex_statictimestep75 \
-    mouse150_v2_transl_statictimestep1 \
-    spheres_cube_dataset_v5_statictimestep1 \
-    standup150_v3_statictimestep75
-do
-    CUDA_VISIBLE_DEVICES=0 python render.py \
-      -m "outputs/irgs_${SCENE}_masks" --eval --diffuse_sample_num 1024
-done
+CUDA_VISIBLE_DEVICES=0 python render.py \
+  -m "outputs/irgs_${SCENE}_masks" --eval --diffuse_sample_num 1024
