41ab5f6e1d2d8dc5657a4dfc420862fb6c8b3327
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-14 08:23:27 +0200

    another relight

--- eval_relighting_enerf.py
+++ eval_relighting_enerf.py
@@ -140,7 +140,7 @@ if __name__ == '__main__':
             mapname = os.environ.get("MAP_NAME", "")
             image_path = os.path.join(args.source_path, mapname, frame.image_path.split("/")[-1] + ".png")
             # NeRF 'transform_matrix' is a camera-to-world transform
-            c2w = np.array(frame.c2w)
+            c2w = np.array(frame.c2w.cpu())
             # change from OpenGL/Blender camera axes (Y up, Z back) to COLMAP (Y down, Z forward)
             c2w[:3, 1:3] *= -1
 
