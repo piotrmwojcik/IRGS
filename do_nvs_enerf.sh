a9f2813a735c58cd1279ceec96899764f2fca15e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-14 16:26:51 +0200

    try to load from ply

--- do_nvs_enerf.sh
+++ do_nvs_enerf.sh
@@ -5,16 +5,6 @@ for SCENE in \
     spheres_cube_dataset_v5_statictimestep1 \
     standup150_v3_statictimestep75
 do
-    for DATA_SUBDIR in \
-        chapel_day_4k_32x16_rot0 \
-        dam_wall_4k_32x16_rot90 \
-        golden_bay_4k_32x16_rot330
-    do
-        export DATA_SUBDIR
-        echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
-
     CUDA_VISIBLE_DEVICES=0 python render.py \
       -m "outputs/irgs_${SCENE}_masks" --eval --diffuse_sample_num 1024
-
-    done
 done
