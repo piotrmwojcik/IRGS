2ba870dcf02301e386f2a0de6965761964bca643
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-15 02:58:15 +0200

    try to load from ply

--- train_stage1_spec.sh
+++ train_stage1_spec.sh
@@ -1,13 +1,14 @@
 for SCENE in \
-    hook150_v4_spec_statictimestep1 \
-    jumpingjacks150_v34_spec_statictimestep75 \
-    mouse150_v3_spec_statictimestep1 \
+    #hook150_v4_spec_statictimestep1 \
+    #jumpingjacks150_v34_spec_statictimestep75 \
+    #mouse150_v3_spec_statictimestep1 \
     spheres_cube_dataset_v6_spec_statictimestep1 \
-    standup150_v4_spec_statictimestep75
+    aaa
+    #standup150_v4_spec_statictimestep75
 do
     for DATA_SUBDIR in \
       chapel_day_4k_1024x512_rot0 \
-      dam_wall_4k_1024x512_rot90 \
+      #dam_wall_4k_1024x512_rot90 \
       golden_bay_4k_1024x512_rot330
     do
         export DATA_SUBDIR
