60797e431bc4bbaaf34f2e99c8bb65f2b7852587
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-05-15 02:37:30 +0200

    try to load from ply

---
+++ do_relight_1024_0_spheres.sh
@@ -0,0 +1,29 @@
+for SCENE in \
+    spheres_cube_dataset_v5_statictimestep1 \
+    aa
+    #standup150_v3_statictimestep75
+do
+
+    pairs=(
+      "chapel_day_4k_32x16_rot0 golden_bay_4k_32x16_rot330"
+      "dam_wall_4k_32x16_rot90 small_harbour_sunset_4k_32x16_rot270"
+      "golden_bay_4k_32x16_rot330 dam_wall_4k_32x16_rot90"
+      "golden_bay_4k_32x16_rot330 chapel_day_4k_32x16_rot0"
+    )
+
+    for pair in "${pairs[@]}"; do
+      read DATA_SUBDIR MAP_NAME <<< "$pair"
+      MAP_PATH="/home/pwojcik/IRGS/data/$SCENE/$MAP_NAME.hdr"
+      echo "Running with DATA_SUBDIR=$DATA_SUBDIR and SCENE=$SCENE"
+      export MAP_PATH
+      export MAP_NAME
+      export SCENE
+      export DATA_SUBDIR
+       #export DATA_SUBDIR
+      echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR and with $MAP_PATH"
+
+      CUDA_VISIBLE_DEVICES=0 python eval_relighting_syn4.py -m outputs/s2_${DATA_SUBDIR}/irgs_$SCENE \
+        --diffuse_sample_num 1024 --light_sample_num 0 --albedo_rescale 2 -e light
+
+    done
+done
