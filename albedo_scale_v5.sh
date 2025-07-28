cfbe5e09fe9a751442e55858725fbafd7ba99bd1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-07-28 09:11:28 +0200

    spec v5

---
+++ albedo_scale_v5.sh
@@ -0,0 +1,19 @@
+for SCENE in \
+    hook150_v5_spec32_statictimestep1 \
+    jumpingjacks150_v5_spec32_statictimestep75 \
+    mouse150_v5_spec32_statictimestep1 \
+    spheres_v5_spec32_statictimestep1 \
+    standup150_v5_spec32_statictimestep75
+do
+    for DATA_SUBDIR in \
+      chapel_day_4k_32x16_rot0 \
+      dam_wall_4k_32x16_rot90 \
+      golden_bay_4k_32x16_rot330
+    do
+        export DATA_SUBDIR
+        echo "Processing SCENE: $SCENE with DATA_SUBDIR: $DATA_SUBDIR"
+
+   CUDA_VISIBLE_DEVICES=0 python compute_albedo_scale_syn4.py \
+      -m outputs/s2_${DATA_SUBDIR}/irgs_$SCENE
+    done
+done
