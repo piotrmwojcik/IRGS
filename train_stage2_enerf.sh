527113c383d6249415ccc016fc2a4b7c9caf7629
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-15 19:33:45 +0200

    env map size

--- train_stage2_enerf.sh
+++ train_stage2_enerf.sh
@@ -3,7 +3,7 @@ SCENE=images_710_780_single_ts
 CUDA_VISIBLE_DEVICES=0 python train.py \
     -s "data/$SCENE" \
     --iterations 20000 \
-    --start_checkpoint_refgs "outputs/${SCENE}_mask/chkpnt50000.pth" \
+    --start_checkpoint_refgs "outputs/${SCENE}/chkpnt50000.pth" \
     --envmap_resolution 128 \
     --lambda_base_color_smooth 2 \
     --lambda_roughness_smooth 2 \
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --lambda_light_smooth 0.0005 \
     --init_roughness_value 0.6 \
     --lambda_light 0.1 \
-    -m "outputs/irgs_${SCENE}_mask" \
+    -m "outputs/irgs_${SCENE}_new" \
     --train_ray \
     --resolution 2
 
