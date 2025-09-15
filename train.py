8dd5fec03975522314006014b4d60891ab75c6c4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-15 19:53:22 +0200

    env map size

--- train.py
+++ train.py
@@ -62,6 +62,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     gaussians.load_ply(
         os.path.join("/home/pwojcik/IRGS/outputs/images_710_780_single_ts/point_cloud/iteration_50000/point_cloud.ply"))
     #gaussians.create_from_pcd(gaussians.point_cloud, self.cameras_extent, args)
+    print('!!! scalign shape 2', gaussians.get_scaling.shape)
     gaussians.training_setup(opt)
     gaussians.build_bvh()
     
