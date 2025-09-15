ad286cbf6aa7bc500f0c669e02b6bfce785afe0c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-15 19:38:32 +0200

    env map size

--- train.py
+++ train.py
@@ -58,10 +58,10 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         (model_params, _) = torch.load(checkpoint_refgs, weights_only=False)
         gaussians.restore_from_refgs(model_params, opt)
 
-    #gaussians.load_ply(
-    #    os.path.join("/home/pwojcik/IRGS/outputs/images_710_780_single_ts/point_cloud/iteration_50000/point_cloud.ply"))
-    #gaussians.create_from_pcd(scene_info.point_cloud, self.cameras_extent, args)
-    #gaussians.training_setup(opt)
+    gaussians.load_ply(
+        os.path.join("/home/pwojcik/IRGS/outputs/images_710_780_single_ts/point_cloud/iteration_50000/point_cloud.ply"))
+    gaussians.create_from_pcd(scene_info.point_cloud, self.cameras_extent, args)
+    gaussians.training_setup(opt)
     gaussians.build_bvh()
     
     if scene.light_rotate:
