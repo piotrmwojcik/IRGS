88ebae50f0bc34fd1273071cea5b3fed214e686c
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-15 22:05:11 +0200

    env map size

--- do_relight_enerf.sh
+++ do_relight_enerf.sh
@@ -5,8 +5,8 @@ SCENE="images_710_780_single_ts"
 maps=(
   #"chapel_day_4k_32x16_rot0"
   #"golden_bay_4k_32x16_rot330"
-  #"test_env_j6_i24"
-  "chapel_day_4k_1024x512_rot0"
+  "test_env_j6_i24"
+  #"chapel_day_4k_1024x512_rot0"
 )
 
 for MAP_NAME in "${maps[@]}"; do
@@ -17,10 +17,10 @@ for MAP_NAME in "${maps[@]}"; do
   export MAP_NAME
   export SCENE
 
-  CUDA_VISIBLE_DEVICES=0 python eval_relighting_enerf.py -m "outputs/irgs_${SCENE}" \
+  CUDA_VISIBLE_DEVICES=0 python eval_relighting_enerf.py -m "outputs/irgs_${SCENE}_new" \
     --diffuse_sample_num 1024 \
-    --light_sample_num 256 \
-    --resolution 4 \
+    --light_sample_num 0 \
+    --resolution 2 \
     --albedo_rescale 0 \
     -e light
 
