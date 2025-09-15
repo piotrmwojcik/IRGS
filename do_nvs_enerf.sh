efa3211abe431f50df9657959ef33ba2d0b38bd0
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-09-15 21:53:50 +0200

    env map size

--- do_nvs_enerf.sh
+++ do_nvs_enerf.sh
@@ -1,4 +1,4 @@
 SCENE="images_710_780_single_ts"
 
 CUDA_VISIBLE_DEVICES=0 python render.py \
-  -m "outputs/irgs_${SCENE}_masks" --eval --diffuse_sample_num 1024
+  -m "outputs/irgs_${SCENE}_new" --eval --diffuse_sample_num 1024
