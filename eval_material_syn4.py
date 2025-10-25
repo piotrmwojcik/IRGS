e7d6f4a00ce25bf0c2a8f79818f4f968ab0d24ad
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-10-25 13:23:26 +0200

    env map size

--- eval_material_syn4.py
+++ eval_material_syn4.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
         gt_albedo = srgb_to_rgb(gt_albedo)
         #gt_albedo = gt_albedo.permute(1, 2, 0)
         #mask = mask.permute(1, 2, 0)
-
+        print('!!! ', frame["file_path"])
         roughness_path = os.path.join(args.source_path, "roughness/")
         #roughness_path = os.path.join(args.source_path, "test/" + frame["file_path"].split("/")[-1] + "_rough.png")
         gt_roughness_np = load_img_rgb(roughness_path)
